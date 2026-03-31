8a697b6d29c9c74b6835d0ff21e5b2f50b7e0131
unknown <violetteyang5331@gmail.com>
2026-03-31 16:07:02 -0400

    3/31 added sign in stuff

--- website/auth.py
+++ website/auth.py
@@ -6,6 +6,42 @@ from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
 
+TEEN_DANCERS = [
+    {"first_name": "Violette", "last_name": "Yang", "birthday":"02:09", "account":False}
+    ]
+
+def match(level, firstName, lastName, birthday):
+    if level == "adult":
+        pass
+    if level == "teen":
+        for i in range(1, len(TEEN_DANCERS)):
+            if firstName == TEEN_DANCERS[i]["first_name"] and lastName == TEEN_DANCERS[i]["last_name"]and birthday == TEEN_DANCERS[i]["birthday"]:
+                TEEN_DANCERS[i]["account"] = True
+                return True
+            else:
+                return False
+    if level == "junior":
+        pass
+    if level == "mini":
+        pass
+    
+def has_acc(level, firstName, lastName, birthday):
+    if level == "adult":
+        pass
+    if level == "teen":
+        for i in range(1, len(TEEN_DANCERS)):
+            if firstName == TEEN_DANCERS[i]["first_name"] and lastName == TEEN_DANCERS[i]["last_name"]and birthday == TEEN_DANCERS[i]["birthday"]:
+                if TEEN_DANCERS[i]["account"] == True:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+    if level == "junior":
+        pass
+    if level == "mini":
+        pass
+
 @auth.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
@@ -34,15 +70,22 @@ def logout():
 @auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
     if request.method == "POST":
+        rqda_level = request.form.get("rqda_level")
         email = request.form.get("email")
         first_name = request.form.get("firstName")
+        last_name = request.form.get("lastName")
+        birthday = request.form.get("birthday_md")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
         
         user = User.query.filter_by(email=email).first()
+        validity = match(rqda_level, first_name, last_name, birthday)
         
+        '''
         if user:
             flash("Email already exists.", category="error")
+        elif rqda_level == "":
+            flash("Please enter an RQDA level.", category="error")
         elif len(email) < 4:
             flash("Email must be greater than 3 characters.", category="error")
         elif len(first_name) < 2:
@@ -51,6 +94,21 @@ def sign_up():
             flash("Passwords must match.", category="error")
         elif len(password1) < 7:
             flash("Password must be at least 7 characters.", category="error")
+        else:
+            new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="pbkdf2:sha256"))
+            db.session.add(new_user)
+            db.session.commit()
+            flash("Account created!", category="success")
+            login_user(user, remember=True)
+            return redirect(url_for("views.home"))
+        '''
+        
+        if user or has_acc(rqda_level, first_name, last_name, birthday):
+            flash("Account already exists.", category="error")
+        elif validity == False:
+            flash("Not a valid dancer, please check your information.", category="error")
+        elif password1 != password2:
+            flash("Passwords must match.", category="error")
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="pbkdf2:sha256"))
             db.session.add(new_user)
--- website/views.py
+++ website/views.py
@@ -7,8 +7,8 @@ import json
 views = Blueprint("views", __name__)
 
 @views.route("/", methods=["GET", "POST"])
-@login_required
 def home():
+    '''
     if request.method == "POST":
         note = request.form.get("note")
         
@@ -19,6 +19,7 @@ def home():
             db.session.add(new_note)
             db.session.commit()
             flash("Note added!", category="success")
+    '''
     
     return render_template("home.html", user=current_user)
 
